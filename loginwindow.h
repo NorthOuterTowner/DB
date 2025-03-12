9bf9247ea771e90a90d5057741bd1b7ee09c7994
ICE5ROKhs <18435003055@163.com>
2025-03-12 11:34:17 +0800

    ui部分登录和主界面

---
+++ loginwindow.h
@@ -0,0 +1,26 @@
+#ifndef LOGINWINDOW_H
+#define LOGINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class loginwindow;
+}
+QT_END_NAMESPACE
+
+class loginwindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    loginwindow(QWidget *parent = nullptr);
+    ~loginwindow();
+
+private slots:
+    void on_button_signin_clicked();
+
+private:
+    Ui::loginwindow *ui;
+};
+#endif // LOGINWINDOW_H
