121edbbc60ebd1766d4bf40d0074e57a460025a6
ICE5ROKhs <18435003055@163.com>
2025-03-16 11:00:27 +0800

    加了图标

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,26 +1,37 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-#include "lexer.h"
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    Lexer *lexer = new Lexer();
-    connect(ui->pushButton, &QPushButton::clicked, this, [=]() {
-        QString rawSQL = ui->inputSQL->toPlainText();
-        lexer->handleRawSQL(rawSQL);  // 调用 Lexer 的槽函数
-    });
+
+
+    //添加图标
+    QIcon yunx_icon("://res/image/yunx.png");
+    ui->yunx->setIcon(yunx_icon);
+
+    QIcon newdb_icon("://res/image/new_db.png");
+    ui->newdb->setIcon(newdb_icon);
+
+    QIcon newtable_icon("://res/image/new_table.png");
+    ui->newtable->setIcon(newtable_icon);
+
+    QIcon clear_icon("://res/image/clear.png");
+    ui->clear->setIcon(clear_icon);
+
+    QIcon exit_icon("://res/image/exit.png");
+    ui->exit->setIcon(exit_icon);
+
+    QIcon save_icon("://res/image/save.png");
+    ui->save->setIcon(save_icon);
+
+
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
-
-void MainWindow::on_pushButton_clicked()
-{
-    std::string rawSQL=ui->inputSQL->toPlainText().toStdString();
-
-}
-
